682d64a26dd9c74c33052f86750a1544a2dee62e
khaled mofed <khaled.mofed1@gmail.com>
2026-01-29 14:57:16 +0200

    update news

--- src/components/footer/FooterStyle1.jsx
+++ src/components/footer/FooterStyle1.jsx
@@ -1,45 +1,57 @@
 "use client";
-import Image from 'next/image';
-import React from 'react';
-import shape7 from '@/assets/img/shape/7.png';
-import shape9 from '@/assets/img/shape/9.png';
-import logoLight from '@/assets/img/logo-light.png';
-import logo2040 from '@/assets/img/logo/2040.webp';
-import { Link } from '@/i18n/routing';
-import { useTranslations } from 'next-intl';
-import SocialShare from '../utilities/SocialShare';
-import FooterNewsLetter from '../form/FooterNewsLetter';
+import Image from "next/image";
+import React from "react";
+import shape7 from "@/assets/img/shape/7.png";
+import shape9 from "@/assets/img/shape/9.png";
+import logoLight from "@/assets/img/logo-light.png";
+import logo2040 from "@/assets/img/logo/2040.webp";
+import { Link } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
+import SocialShare from "../utilities/SocialShare";
+import FooterNewsLetter from "../form/FooterNewsLetter";
 
 const FooterStyle1 = ({ shape, shapeClass, logo, formStyle }) => {
-    const t = useTranslations('common');
-    const tFooter = useTranslations('footer');
-    return (
-        <>
-            <footer className="bg-dark text-light">
-                <div className="footer-shape">
-                    <div className={`item ${shapeClass}`}>
-                        {shape ? <Image src={shape} alt="Shape" className="toty-logo" /> : <Image src={shape7} alt="Shape" className="toty-logo" />}
-                    </div>
-                    <div className="item">
-                        <Image src={shape9} alt="Shape" className="toty-logo" />
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="f-items relative pt-40 pb-40 pt-xs-0 pb-xs-50">
-                        <div className="row">
-                            <div className="col-lg-4 col-md-6 footer-item pr-50 pr-xs-15">
-                                <div className="f-item about">
-                                    <div className="logos-container">
-                                    <Link href="/" className="logo-container-text">
-                                        {logo ? <Image className="logo" src={logo} alt="Logo " /> : <Image className="logo" src={logoLight} alt="Logo" />}
-                                        <div className="logo-text">{t('companyNameLine1')} <br /> {t('companyNameLine2')}</div>
-                                    </Link>
-                                    <Image src={logo2040} alt="Logo 2040" className="logo2040" />
-                                    </div>
-                                    <p className='mb-0'>
-                                        {tFooter('description')}
-                                    </p>
-                                    {/* <div className="opening-hours">
+  const t = useTranslations("common");
+  const tFooter = useTranslations("footer");
+  return (
+    <>
+      <footer className="bg-dark text-light">
+        <div className="footer-shape">
+          <div className={`item ${shapeClass}`}>
+            {shape ? (
+              <Image src={shape} alt="Shape" className="toty-logo" />
+            ) : (
+              <Image src={shape7} alt="Shape" className="toty-logo" />
+            )}
+          </div>
+          <div className="item">
+            <Image src={shape9} alt="Shape" className="toty-logo" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="f-items relative pt-40 pb-40 pt-xs-30 pb-xs-30">
+            <div className="row">
+              <div className="col-lg-4 col-md-6 footer-item pr-50 pr-xs-15">
+                <div className="f-item about">
+                  <div className="logos-container">
+                    <Link href="/" className="logo-container-text">
+                      {logo ? (
+                        <Image className="logo" src={logo} alt="Logo " />
+                      ) : (
+                        <Image className="logo" src={logoLight} alt="Logo" />
+                      )}
+                      <div className="logo-text">
+                        {t("companyNameLine1")} <br /> {t("companyNameLine2")}
+                      </div>
+                    </Link>
+                    <Image
+                      src={logo2040}
+                      alt="Logo 2040"
+                      className="logo2040"
+                    />
+                  </div>
+                  <p className="mb-0">{tFooter("description")}</p>
+                  {/* <div className="opening-hours">
                                         <h5>Opening Hours</h5>
                                         <ul>
                                             <li>
@@ -54,16 +66,18 @@ const FooterStyle1 = ({ shape, shapeClass, logo, formStyle }) => {
                                             </li>
                                         </ul>
                                     </div> */}
-                                </div>
-                            </div>
-                            <div className="col-lg-2 col-md-6 footer-item">
-                                <div className="f-item link">
-                                    {/* <h4 className="widget-title">Our Company</h4> */}
-                                    <ul>
-                                        <li>
-                                            <a href="mailto:info@zainom.com" target='_blank'>info@zainom.com</a>
-                                        </li>
-                                        {/* <li>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-6 footer-item">
+                <div className="f-item link">
+                  {/* <h4 className="widget-title">Our Company</h4> */}
+                  <ul>
+                    <li>
+                      <a href="mailto:info@zainom.com" target="_blank">
+                        info@zainom.com
+                      </a>
+                    </li>
+                    {/* <li>
                                             <Link href="/contact-us">Help Center</Link>
                                         </li>
                                         <li>
@@ -75,17 +89,19 @@ const FooterStyle1 = ({ shape, shapeClass, logo, formStyle }) => {
                                         <li>
                                             <Link href="/contact-us">Contact</Link>
                                         </li> */}
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="col-lg-2 col-md-6 footer-item">
-                                <div className="f-item link">
-                                    {/* <h4 className="widget-title">Our Services</h4> */}
-                                    <ul>
-                                        <li>
-                                            <a href="tel:+96872720046" target='_blank' dir="ltr">+968 7272 0046</a>
-                                        </li>
-                                        {/* <li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-6 footer-item">
+                <div className="f-item link">
+                  {/* <h4 className="widget-title">Our Services</h4> */}
+                  <ul>
+                    <li>
+                      <a href="tel:+96872720046" target="_blank" dir="ltr">
+                        +968 7272 0046
+                      </a>
+                    </li>
+                    {/* <li>
                                             <Link href="/services-details/1">Business planning</Link>
                                         </li>
                                         <li>
@@ -97,49 +113,55 @@ const FooterStyle1 = ({ shape, shapeClass, logo, formStyle }) => {
                                         <li>
                                             <Link href="/services-details/1">Insurance strategy</Link>
                                         </li> */}
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6 footer-item">
-                                {/* <h4 className="widget-title">Newsletter</h4>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 footer-item">
+                {/* <h4 className="widget-title">Newsletter</h4>
                                 <p>
                                     Join our subscribers list to get the latest <br /> news and special offers.
                                 </p>
                                 <div className={`f-item newsletter ${formStyle}`}>
                                     <FooterNewsLetter />
                                 </div> */}
-                                <ul className="footer-social">
-                                    <SocialShare />
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="footer-bottom">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-6">
-                                <p>&copy; {t('copyright')} {(new Date().getFullYear())}. {t('allRightsReserved')} <a href="#" target='_blank'>{t('companyName')}</a></p>
-                            </div>
-                            <div className="col-lg-6 text-end">
-                                <ul>
-                                    <li>
-                                        <Link href="/terms">{t('terms')}</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/privacy">{t('privacy')}</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/contact-us">{t('support')}</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        </>
-    );
+                <ul className="footer-social">
+                  <SocialShare />
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <p>
+                  &copy; {t("copyright")} {new Date().getFullYear()}.{" "}
+                  {t("allRightsReserved")}{" "}
+                  <a href="#" target="_blank">
+                    {t("companyName")}
+                  </a>
+                </p>
+              </div>
+              <div className="col-lg-6 text-end">
+                <ul>
+                  <li>
+                    <Link href="/terms">{t("terms")}</Link>
+                  </li>
+                  <li>
+                    <Link href="/privacy">{t("privacy")}</Link>
+                  </li>
+                  <li>
+                    <Link href="/contact-us">{t("support")}</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default FooterStyle1;
--- src/components/header/MainHeaderStyle1.jsx
+++ src/components/header/MainHeaderStyle1.jsx
@@ -1,61 +1,89 @@
-"use client"
-import React from 'react';
-import MainMenu from './MainMenu';
-import ZainMenu from './ZainMenu';
-import HeaderLogo from './HeaderLogo';
-import { Link } from '@/i18n/routing';
-import { useTranslations } from 'next-intl';
-import useStickyMenu from '../hooks/useStickyMenu';
-import useSubMenuToggle from '../hooks/useSubMenuToggle';
-import useSidebarMenu from '../hooks/useSidebarMenu';
-import Image from 'next/image';
-import logo from '@/assets/img/logo.png';
-import LanguageSwitcher from '../utilities/LanguageSwitcher';
+"use client";
+import React from "react";
+import MainMenu from "./MainMenu";
+import ZainMenu from "./ZainMenu";
+import HeaderLogo from "./HeaderLogo";
+import { Link } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
+import useStickyMenu from "../hooks/useStickyMenu";
+import useSubMenuToggle from "../hooks/useSubMenuToggle";
+import useSidebarMenu from "../hooks/useSidebarMenu";
+import Image from "next/image";
+import logo from "@/assets/img/logo.png";
+import LanguageSwitcher from "../utilities/LanguageSwitcher";
 
 const MainHeaderStyle1 = () => {
-    const t = useTranslations('common');
-    const isMenuSticky = useStickyMenu();
-    const toggleSubMenu = useSubMenuToggle();
-    const { isOpen, openMenu, closeMenu } = useSidebarMenu();
+  const t = useTranslations("common");
+  const isMenuSticky = useStickyMenu();
+  const toggleSubMenu = useSubMenuToggle();
+  const { isOpen, openMenu, closeMenu } = useSidebarMenu();
 
-    return (
-        <>
-            <header>
-                <nav className={`navbar mobile-sidenav navbar-style-one navbar-sticky navbar-default validnavs white navbar-fixed on menu-center no-full ${isMenuSticky ? 'sticked' : 'no-background'} ${isOpen ? "navbar-responsive force-sticky" : ""}`}>
-                    <div className="container">
-                        <div className="row align-center">
-                            <div className="col-xl-2 col-lg-3 col-md-2 col-sm-1 col-1">
-                                <HeaderLogo openMenu={openMenu} />
-                            </div>
-                            <div className="col-xl-6 offset-xl-1 col-lg-6 col-md-4 col-sm-4 col-4">
-                                <div className={`collapse navbar-collapse collapse-mobile ${isOpen ? "show" : ""}`} id="navbar-menu">
-                                    <Image src={logo} alt="Logo" />
-                                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu" onClick={closeMenu}>
-                                        <i className="fa fa-times"></i>
-                                    </button>
-                                    <ZainMenu navbarPlacement="navbar-center" isOpen={isOpen} closeMenu={closeMenu} toggleSubMenu={toggleSubMenu} />
-                                </div>
-                            </div>
-                            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-7 col-7">
-                                <div className="attr-right">
-                                <LanguageSwitcher />
-                                    <div className="attr-nav">
-                                        <ul>
-                                            <li className="button">
-                                                <Link href="/contact-us">{t('getConsultant')}</Link>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`overlay-screen ${isOpen ? "opened" : ""}`} onClick={closeMenu}></div>
+  return (
+    <>
+      <header>
+        <nav
+          className={`navbar mobile-sidenav navbar-style-one navbar-sticky navbar-default validnavs white navbar-fixed on menu-center no-full ${isMenuSticky ? "sticked" : "no-background"} ${isOpen ? "navbar-responsive force-sticky" : ""}`}
+        >
+          <div className="container">
+            <div className="row align-center">
+              <div className="col-xl-2 col-lg-3 col-md-2 col-sm-1 col-1">
+                <HeaderLogo openMenu={openMenu} />
+              </div>
+              <div className="col-xl-6 offset-xl-1 col-lg-6 col-md-4 col-sm-4 col-4">
+                <div
+                  className={`collapse navbar-collapse collapse-mobile ${isOpen ? "show" : ""}`}
+                  id="navbar-menu"
+                >
+                  <Image src={logo} alt="Logo" />
+                  <button
+                    type="button"
+                    className="navbar-toggle"
+                    data-toggle="collapse"
+                    data-target="#navbar-menu"
+                    onClick={closeMenu}
+                  >
+                    <i className="fa fa-times"></i>
+                  </button>
+                  <ZainMenu
+                    navbarPlacement="navbar-center"
+                    isOpen={isOpen}
+                    closeMenu={closeMenu}
+                    toggleSubMenu={toggleSubMenu}
+                  />
+                  <div className="attr-right-mobile attr-right">
+                    <LanguageSwitcher />
+                    <div className="attr-nav">
+                      <ul>
+                        <li className="button">
+                          <Link href="/contact-us">{t("getConsultant")}</Link>
+                        </li>
+                      </ul>
                     </div>
-                </nav>
-            </header>
-        </>
-    );
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-3 col-lg-3 col-md-6 col-sm-7 col-7">
+                <div className="attr-right">
+                  <LanguageSwitcher />
+                  <div className="attr-nav">
+                    <ul>
+                      <li className="button">
+                        <Link href="/contact-us">{t("getConsultant")}</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              className={`overlay-screen ${isOpen ? "opened" : ""}`}
+              onClick={closeMenu}
+            ></div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default MainHeaderStyle1;
